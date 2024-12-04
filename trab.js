a9fbc9e5c11a967f8d8452b70642976b24124c28
brunnapaganini <brunnapaganini@gmail.com>
2024-12-04 20:36:51 -0300

    Add files via upload

---
+++ trab.js
@@ -0,0 +1,5 @@
+let formulario = document.getElementById('contato');
+        
+        formulario.addEventListener('submit', function(event) {
+            alert('Formulário enviado com sucesso!');
+        });
